a1d2656f4afa334384d05b3450293cad01defdaf
Jesus Rodriguez <jesus.rodriguez@cpcc.edu>
2020-12-06 16:09:38 -0500

    change sh to bash in start script

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 echo "#By changing the setting below to TRUE you are indicating your agreement to our EULA (https://account.mojang.com/documents/minecraft_eula)." > minecraft/eula.txt && \
 echo "$(date)" >> minecraft/eula.txt && \
