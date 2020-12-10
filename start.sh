73805c66f0805e9789a2010ce5e9708549f33aae
Jesus Rodriguez <jesus.rodriguez@cpcc.edu>
2020-12-10 11:49:43 -0500

    add full support for raspberry pi and update readme

--- start.sh
+++ start.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-echo "#By changing the setting below to TRUE you are indicating your agreement to our EULA (https://account.mojang.com/documents/minecraft_eula)." > minecraft/eula.txt && \
-echo "$(date)" >> minecraft/eula.txt && \
-echo "eula=TRUE" >> minecraft/eula.txt
+bash eula.sh
 
 docker-compose up -d
